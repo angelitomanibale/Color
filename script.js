3f1585976ec4f84b92c97d0cca60b7713499fa3a
Angelito Manibale <angelitoomanibale@gmail.com>
2023-08-22 07:23:28 +0800

    V1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ input = prompt('>>> ')
 text = input.split(': ')
 text.shift()
 text = text.toString()
-Colors = [{name: 'Yellow', code: '1f7e8'}, {name: 'Blue', code: '1f499'}, {name: 'Green', code: '1f7e9'}, {name: 'Red', code: 'fe0f'}, {name: 'Orange', code: '1f7e7'}, {name: 'White', code: '26aa'}, {name: 'Black', code: '1f5a4'}]
+Colors = [{name: 'Yellow', code: '1f7e8'}, {name: 'Blue', code: '1f499'}, {name: 'Green', code: '1f7e9'}, {name: 'Red', code: 'fe0f'}, {name: 'Orange', code: '1f7e7'}, {name: 'White', code: '26aa'}, {name: 'Black', code: '1f5a4'}, {name: 'Brown', code: '1f90e'}]
 function emojiUnicode (emoji) {
     var comp;
     if (emoji.length === 1) {
@@ -18,18 +18,19 @@ function emojiUnicode (emoji) {
     }
     return comp.toString("16");
 };
-emoji = emojiUnicode(text)
-console.log(emoji)
-find(emoji)
 
 function find(emoji){
-    condition = false
     for (let i = 0; i < Colors.length; i++) {
         if (emoji == Colors[i].code){
+            console.log(Colors[i].name)
             output.innerHTML = Colors[i].name
-        }   
+        }else{
+            console.log(emoji)
+        }
     }
 }
+emoji = emojiUnicode(text)
+find(emoji)
 yellow = '1f7e8'
 blue = '1f499'
 green = '1f7e9'
